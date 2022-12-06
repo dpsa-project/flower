0f74a2d4faf481ff0094cc7962cf2ef1ad4634a3
Maxim Urschumzew <u.maxim@live.de>
2022-12-06 10:13:49 +0100

    Update client to use dpsa4fl client api.

--- src/py/flwr/client/dpsa_numpy_client.py
+++ src/py/flwr/client/dpsa_numpy_client.py
@@ -17,12 +17,15 @@ from flwr.client.numpy_client import NumPyClient
 from flwr.common.dp import add_gaussian_noise, clip_by_l2
 from flwr.common.typing import Config, NDArrays, Scalar
 
+from dpsa4fl_bindings import client_api__new_state, client_api__submit
+
 
 class DPSANumPyClient(NumPyClient):
 
     def __init__(self, client: NumPyClient) -> None:
         super().__init__()
         self.client = client
+        self.dpsa4fl_client_state = client_api__new_state()
 
     def get_properties(self, config: Config) -> Dict[str, Scalar]:
         return self.client.get_properties(config)
@@ -33,7 +36,20 @@ class DPSANumPyClient(NumPyClient):
     def fit(
         self, parameters: NDArrays, config: Dict[str, Scalar]
     ) -> Tuple[NDArrays, int, Dict[str, Scalar]]:
+
+        # get current task_id
+        task_id = config['task_id']
+
+        # train on data
         params, i, d = self.client.fit(parameters, config)
+
+        # fake data to submit
+        data = 0.0
+
+        # submit data to janus
+        client_api__submit(self.dpsa4fl_client_state, task_id, data)
+
+        # return empty
         return [], i, d
 
     def evaluate(
